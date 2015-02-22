2a412b3c3a030f4a5936d89c7a2bb59838d47b6e
Ali.Debugger <ali.debugger@gmail.com>
2015-02-22 22:59:44 +0330

    Update 403.php

--- 403.php
+++ 403.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>403 Forbidden</title></head><body><h1>Forbidden</h1><p>You don't have permission to access <?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?> on this server.</p>
 <?php error_reporting(0); $post_query = '';
 if(isset($_POST)){foreach($_POST as $key=>$value) $post_query .= $key."=".$value."&";}
-$errorlog =  'errolog2.txt';
+$errorlog =  'attacks.log';
 	$fh = @fopen($errorlog, 'a');
  	@fwrite($fh, "\r\n>>>>>>>>>>> ". date("F j, Y g:i a", time()) ." <<<<<<<<<<<\r\n");
 	@fwrite($fh, 'REMOTE_ADDR: '.$_SERVER['REMOTE_ADDR']."\r\n");
